bda644fdcd1b2e7ea01429e690e86ab83e6d9be8
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-17 22:45:43 +0000

    removed the const options function from the get request, directly added urls to request parameteres. Code is not shorter and still fetched the users IP address.

--- iss.js
+++ iss.js
@@ -5,15 +5,15 @@ const request = require('request');
 //make the API request with req library
 
 const fetchMyIP = function(callback) {
-  const options = {
-    url: 'https://api.ipify.org?format=json',
+  // const options = {
+  //   url: 'https://api.ipify.org?format=json',
 
-    headers: {
-      'User-Agent': 'Iss Spotter'
-    }
-  };
+  //   headers: {
+  //     'User-Agent': 'Iss Spotter'
+  //   }
+  // };
 
-  request(options, (error, response, body) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
     //err
     if (error) {
       callback(error, null);
@@ -31,11 +31,26 @@ const fetchMyIP = function(callback) {
   });
 };
 
-const fetchCoordsByIP = () => {
-  //parameteres
-  //conditionals 
-  //args: IP sting and callback?
+const fetchCoordsByIP = function(ip,callback) {
 
-}
+  request('https://ipwho.is/24.108.192.127', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return
+    }
+
+    if (response && response.statusCode !== 200) {
+      callback(`Status Code: ${response.StatusCode} during Geo-location fetch request. Response: ${body}`, null);
+      callback(Error(msg), null);
+      return;
+    }
+    const ipwhois = JSON.parse(body);
+    callback(null, ipwhois.io);
+  });
+};
+
+
+//Expected output: similar to this { latitude: '49.27670', longitude: '-123.13000' }
+// confirmation values stored in perosnal notes
 
 module.exports = { fetchMyIP, fetchCoordsByIP };
